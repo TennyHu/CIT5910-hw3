b7989edeed69fb4e788f1341cc05ac6d816f80a5
tennyhu <tennyhu@hotmail.com>
2025-09-25 16:52:24 -0400

    changed getSum() helper and added getFactors() helper

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        PositiveInteger pi = new PositiveInteger(7);
+        PositiveInteger pi = new PositiveInteger(10);
         System.out.println(pi.isPerfect());
     }
 }
--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -7,33 +7,54 @@ public class PositiveInteger {
     }
 
     /*
-    Method: get sum of all the factors, except for 1, return 0
+    Method: return an int array, where res[0] has the sum of factors, res[1] counts the number of factors
+    if the number is 1, return 0 for the sum
      */
-    public int getSum() {
+    public int[] getSum() {
         int sum = 0;
+        int count = 0;
         for (int i = 1; i * i <= num; i++) {
             if (num % i == 0) { // i is a factor of num
                 sum += i;
-                System.out.println(i);
+                count++;
+                System.out.println("factor is: " + i);
                 if (i != num / i && num != num / i) {
                     // if i is a factor, num / i is also a factor
                     sum += num / i;
-                    System.out.println(num/i);
+                    count++;
+                    System.out.println("factor is: " + num/i);
                 }
             }
         }
         if (sum == 1) {
-            return 0;
-        } else {
-            return sum;
+            sum = 0;
         }
+        System.out.println("count is: " + count);
+        return new int[]{sum, count};
+    }
+
+    public int[] getFactors() {
+        int count = getSum()[1];
+        int[] factors = new int[count];
+        int pointer = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                factors[pointer] = i;
+                pointer++;
+                if (i != num / i && num != num / i) {
+                    factors[pointer] = num / i;
+                    pointer++;
+                }
+            }
+        }
+        return factors;
     }
 
     /*
     Method: check if the sum of all factors is equal to the number itself -> if so, perfect number
      */
     public boolean isPerfect() {
-        int sum = getSum();
+        int sum = getSum()[0];
         if (sum == num) {
             return true;
         } else {
@@ -45,7 +66,7 @@ public class PositiveInteger {
     Method: check if the sum of all factors is larger than the number itself -> if so, abundant number
      */
     public boolean isAbundant() {
-        int sum = getSum();
+        int sum = getSum()[0];
         System.out.println(sum);
         if (sum > num) {
             return true;
