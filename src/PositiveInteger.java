6d1eb831adc1f24bbfcaf2c145878665355e9bd0
tennyhu <tennyhu@hotmail.com>
2025-09-25 16:32:11 -0400

    added getSum() helper and done isAbundant()

--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -6,30 +6,39 @@ public class PositiveInteger {
         num = number;
     }
 
-    public boolean isPerfect() {
+    public int getSum() {
         int sum = 0;
         for (int i = 1; i * i < num; i++) {
             if (num % i == 0) { // i is a factor of num
                 sum += i;
-                System.out.println(i);
+                //System.out.println(i);
                 if (i != num / i && num != num / i) {
                     // if i is a factor, num / i is also a factor
                     sum += num / i;
-                    System.out.println(num/i);
+                    //System.out.println(num/i);
                 }
             }
         }
-        System.out.println("The sum of the factors is: " + sum);
+        return sum;
+    }
+
+    public boolean isPerfect() {
+        int sum = getSum();
         if (sum == num) {
             return true;
         } else {
             return false;
         }
-
     }
 
     public boolean isAbundant() {
-        return false;
+        int sum = getSum();
+        System.out.println(sum);
+        if (sum > num) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean isNarcissistic() {
