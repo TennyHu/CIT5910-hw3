7da7f2acf46ee1d8d7e731fe9aee3cebee4cebcb
tennyhu <tennyhu@hotmail.com>
2025-09-27 12:41:16 -0400

    modify isPerfect(), eliminate num = 1

--- src/PositiveInteger.java
+++ src/PositiveInteger.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math.*;
 
 public class PositiveInteger {
 
@@ -13,24 +14,20 @@ public class PositiveInteger {
     Method: return an int array, where res[0] has the sum of factors, res[1] counts the number of factors
     if the number is 1, return 0 for the sum
      */
-    public int getSum() {
+    private int getSum() {
         int sum = 0;
         for (int i = 1; i * i <= num; i++) {
             if (num % i == 0) { // i is a factor of num
                 sum += i;
-                System.out.println("factor is: " + i);
+//                System.out.println("factor is: " + i);
                 if (i != num / i && num != num / i) {
                     // if i is a factor, num / i is also a factor
                     sum += num / i;
-                    System.out.println("factor is: " + num/i);
+//                    System.out.println("factor is: " + num/i);
                 }
             }
         }
-        if (sum == 1) {
-            return 0;
-        } else {
-            return sum;
-        }
+        return sum;
     }
 
     /*
@@ -38,11 +35,11 @@ public class PositiveInteger {
      */
     public boolean isPerfect() {
         int sum = getSum();
-        if (sum == num) {
-            return true;
-        } else {
+        if (num == 1) {
             return false;
         }
+
+        return sum == num;
     }
 
     /*
@@ -50,7 +47,7 @@ public class PositiveInteger {
      */
     public boolean isAbundant() {
         int sum = getSum();
-        System.out.println(sum);
+//        System.out.println(sum);
 
         if (sum > num) {
             return true;
@@ -64,9 +61,9 @@ public class PositiveInteger {
      */
     public boolean isNarcissistic() {
         int count = 0;
-        int sum = 0;
+        double sum = 0;
         int input = num;
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
 
         while (input > 0) {
             // acquire each digit
@@ -78,12 +75,12 @@ public class PositiveInteger {
         }
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.println("this is our individual number: " + list.get(i));
+//            System.out.println("this is our individual number: " + list.get(i));
             // compute the sum
             sum += Math.pow(list.get(i), count);
         }
 
-        System.out.println("This is count: " + count);
+//        System.out.println("This is count: " + count);
         if (sum == num) {
             return true;
         } else {
